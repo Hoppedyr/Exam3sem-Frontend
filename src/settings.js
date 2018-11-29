a7de39f9b8af1a3e344c7505f8628b7eb55d7438
cl0tCh <screenchok@gmail.com>
2018-11-29 12:42:09 +0100

    fix

--- src/settings.js
+++ src/settings.js
@@ -6,7 +6,7 @@ export const deleteRestaurant = "" // not Done
 export const getRestaurantById = "http://localhost:8084/durumbo/api/info/resturant/"
 export const getRestaurants = "http://localhost:8084/durumbo/api/info/durumborestaurants/"
 
-export const CRUDTABLEURL = "http://localhost:3456/api"; // DATASERVER CHANGE TO BACKEND
+//export const CRUDTABLEURL = "http://localhost:3456/api"; // DATASERVER CHANGE TO BACKEND
 
 // export const LogInURL = "https://andreasheick.dk/durumbo/";
 // export const URLRestaurants = "https://andreasheick.dk/durumbo/api/info/allrestaurants";
@@ -16,9 +16,5 @@ export const CRUDTABLEURL = "http://localhost:3456/api"; // DATASERVER CHANGE TO
 export const LogInURL = "http://localhost:8084/durumbo";
 export const URLRestaurants = "http://localhost:8084/durumbo/api/info/allrestaurants/";
 export const URLDurumboRestaurants = "http://localhost:8084/durumbo/api/info/durumborestaurants/";
-
-export const LogInURL = "http://localhost:8080/durumbo";
-export const URLRestaurants = "http://localhost:8080/durumbo/api/info/allrestaurants/";
-export const URLDurumboRestaurants = "http://localhost:8080/durumbo/api/info/durumborestaurants/";
 
 
