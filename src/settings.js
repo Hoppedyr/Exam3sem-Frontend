5200c32fc51981ee52f0889db48bf547660b87fa
Andreas <andreas848@hotmail.com>
2018-11-29 10:14:25 +0100

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 export const CRUDTABLEURL = "http://localhost:3456/api"; // DATASERVER CHANGE TO BACKEND
 // export const LogInURL = "https://andreasheick.dk/durumbo/";
-// export const URLRestaurants = "https://andreasheick.dk/durumbo/api/info/all";
+// export const URLRestaurants = "https://andreasheick.dk/durumbo/api/info/allrestaurants";
 // export const URLDurumboRestaurants = "https://andreasheick.dk/durumbo/api/info/durumborestaurants/";
 
 export const LogInURL = "http://localhost:8080/durumbo";
-export const URLRestaurants = "http://localhost:8080/durumbo/api/info/all/";
+export const URLRestaurants = "http://localhost:8080/durumbo/api/info/allrestaurants/";
 export const URLDurumboRestaurants = "http://localhost:8080/durumbo/api/info/durumborestaurants/";
