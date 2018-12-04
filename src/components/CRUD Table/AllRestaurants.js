09f7405c18b98829d075444ef60b235637103cb4
Andreas <andreas848@hotmail.com>
2018-12-04 13:37:20 +0100

    CRUDTable fix

--- src/components/CRUD Table/AllRestaurants.js
+++ src/components/CRUD Table/AllRestaurants.js
@@ -6,11 +6,11 @@ export default function AllRestaurants(props) {
   const list = restaurants.map((restaurant) =>
     <tr key={restaurant.id}>
       <td>{restaurant.id}</td>
-      <td style={{ maxWidth: 40, wordWrap: 'break-word' }}>{restaurant.restName}</td>
-      <td style={{ maxWidth: 40, wordWrap: 'break-word' }}>{restaurant.phone}</td>
+      <td style={{ maxWidth: 50, wordWrap: 'break-word' }}>{restaurant.restName}</td>
+      <td style={{ maxWidth: 50, wordWrap: 'break-word' }}>{restaurant.phone}</td>
       <td>{restaurant.foodType}</td>
-      <td style={{ maxWidth: 40, wordWrap: 'break-word' }}>{restaurant.website}</td>
-      <td style={{ maxWidth: 40, wordWrap: 'break-word' }}>{restaurant.pictureUrl}</td>
+      <td style={{ maxWidth: 50, wordWrap: 'break-word' }}>{restaurant.website}</td>
+      <td style={{ maxWidth: 50, wordWrap: 'break-word' }}>{restaurant.pictureUrl}</td>
       <td>{restaurant.street}</td>
       <td>{restaurant.cityInfo.city}</td>
       <td>{restaurant.cityInfo.zip}</td>
@@ -21,7 +21,7 @@ export default function AllRestaurants(props) {
     <div>
       <h3 style={{textAlign: "center"}}>Number of Restaurants: {props.restaurants.length}</h3>
       <hr/>
-      <table className="table">
+      <table className="table" >
         <thead>
           <tr><th >Id</th><th>Name</th><th>Phone</th><th>Food Type</th><th>website</th><th>pictureURL</th><th>street</th><th>City</th><th>Zip</th><th>Options</th></tr>
         </thead>
--- src/components/CRUD Table/CRUDTable.js
+++ src/components/CRUD Table/CRUDTable.js
@@ -69,15 +69,13 @@ class CRUDTable extends Component {
     return (
       <div style={{ margin: 20 }}>
         <div className="row">
-          <div className="col-md-7">
-          <h3 style={{backgroundColor: "#000000", fontWeight: "bold", borderRadius: '0.25em', textAlign: 'center', color: '#BAFBFF', border: '1px solid grey', padding: '0.3em' }}>Restaurant Manager</h3>
-            <AllRestaurants restaurants={this.state.restaurants} onEdit={this.onEdit} onDelete={this.onDelete} />
-          </div>
-          <div className="col-md-1">
-          </div>
-          <div className="col-md-4" >
-          <h3 style={{backgroundColor: "#000000", fontWeight: "bold", borderRadius: '0.25em', textAlign: 'center', color: '#BAFBFF', border: '1px solid grey', padding: '0.3em' }}>Add / Edit Restaurants</h3>
+          <div style={{ width: "30%", marginRight: 20 }}>
+            <h3 style={{ backgroundColor: "#000000", fontWeight: "bold", borderRadius: '0.25em', textAlign: 'center', color: '#BAFBFF', border: '1px solid grey', padding: '0.3em' }}>Add / Edit Restaurants</h3>
             <AddEditRestaurant key={this.state.editRestaurant.id} onAdd={this.onAdd} onEdit={this.onEdit} onEditSubmit={this.onEditSubmit} verifyID={this.verifyID} restaurant={this.state.editRestaurant} />
+          </div>
+          <div>
+            <h3 style={{ backgroundColor: "#000000", fontWeight: "bold", borderRadius: '0.25em', textAlign: 'center', color: '#BAFBFF', border: '1px solid grey', padding: '0.3em' }}>Restaurant Manager</h3>
+            <AllRestaurants restaurants={this.state.restaurants} onEdit={this.onEdit} onDelete={this.onDelete} />
           </div>
         </div>
 
